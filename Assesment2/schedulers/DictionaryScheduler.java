834bfa42c7a82e380f72a07789ea14e97e408d37
Sourav Sharma <67095793+srv9211@users.noreply.github.com>
2022-10-05 05:28:43 +0530

    Add files via upload

---
+++ Assesment2/schedulers/DictionaryScheduler.java
@@ -0,0 +1,68 @@
+package com.akqa.aem.training.aem201.core.schedulers;
+
+import com.akqa.aem.training.aem201.core.models.DictionaryModel;
+
+import org.apache.sling.commons.scheduler.ScheduleOptions;
+import org.apache.sling.commons.scheduler.Scheduler;
+
+import java.text.SimpleDateFormat;
+
+import org.osgi.service.component.annotations.*;
+import org.osgi.service.metatype.annotations.Designate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+@Component(immediate = true, service = Runnable.class)
+@Designate(ocd = DictionarySchedulerConfiguration.class)
+public class DictionaryScheduler implements Runnable {
+    private static final Logger LOG = LoggerFactory.getLogger(DictionaryScheduler.class);
+
+    private int schedulerId;
+
+    @Reference
+    private Scheduler scheduler;
+
+    @Activate
+    protected void activate(DictionarySchedulerConfiguration config) {
+        schedulerId = config.schedulerName().hashCode();
+        addScheduler(config);
+    }
+
+    @Deactivate
+    protected void deactivate(DictionarySchedulerConfiguration config) {
+        removeScheduler();
+    }
+
+    protected void removeScheduler() {
+        scheduler.unschedule(String.valueOf(schedulerId));
+    }
+
+    protected void addScheduler(DictionarySchedulerConfiguration config) {
+        ScheduleOptions scheduleOptions = scheduler.EXPR(config.cronExpression());
+        scheduleOptions.name(String.valueOf(schedulerId));
+        scheduler.schedule(this, scheduleOptions);
+    }
+    public static int randomIndex;
+    public static String wordUpdatedTime;
+    private Queue<Integer> queue = new ArrayDeque<>();
+
+    @Override
+    public void run() {
+        do {
+            randomIndex = (int)(Math.random()*DictionaryModel.dictionarySize);
+        } while(queue.contains(randomIndex));
+
+        // word will not be repeated if it has been showed in  previous last 10 words.
+        queue.add(randomIndex);
+
+        if(queue.size()>10) {
+            queue.remove();
+        }
+
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+        Date date = new Date();
+        wordUpdatedTime = formatter.format(date);
+    }
+}
