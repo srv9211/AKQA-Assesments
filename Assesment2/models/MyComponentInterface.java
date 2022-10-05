cf4de0792f529e7990f0b7966cf2efbb6a5054a6
Sourav Sharma <67095793+srv9211@users.noreply.github.com>
2022-10-05 07:10:18 +0530

    Delete Assesment2 directory

--- Assesment2/models/MyComponentInterface.java
+++
@@ -1,8 +0,0 @@
-package com.akqa.aem.training.aem201.core.models;
-
-public interface MyComponentInterface {
-    String getFirstName();
-    String getLastName();
-    String getAddress();
-    String getPageTitle();
-}
